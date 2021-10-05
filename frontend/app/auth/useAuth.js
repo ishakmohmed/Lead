6342408b9efcb5167cc688aba9a894e9f5c7e729
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-10-05 14:07:42 +0800

    Cleaned up auth folder.

--- frontend/app/auth/useAuth.js
+++ frontend/app/auth/useAuth.js
@@ -1,14 +1,15 @@
 import { useContext } from "react";
 import jwtDecode from "jwt-decode";
+
 import AuthContext from "./context";
 import authStorage from "./storage";
 
 export default useAuth = () => {
   const { user, setUser } = useContext(AuthContext);
-
   const logIn = (authToken) => {
     try {
       const user = jwtDecode(authToken);
+      
       setUser(user);
       authStorage.storeToken(authToken);
     } catch (error) {
