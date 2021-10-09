345c5282f54bd3bac67cdb80098fbd5a4c9327d6
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-10-09 15:15:19 +0800

    Added welcome screen.

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Siuuuu!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View>{/* <WelcomeScreen /> */}</View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ frontend/app/screens/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+
+// import Button from "../components/Button";
+// import routes from "../navigation/routes";
+import colors from "../config/colors";
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>This is Welcome Screen</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
