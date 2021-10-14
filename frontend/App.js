5a96792d1b728cd127ec1da514ba9bc040834923
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-10-14 23:13:52 +0800

    Made animation component by Lottie work by removing extra parent view.

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { View } from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View>
+    <>
       <WelcomeScreen />
-    </View>
+    </>
   );
 }
